9abd91432539b514ec07eb96cf362f21cbd1d196
mihinduranasinghe <mihindu.21523@gmail.com>
2020-01-11 18:21:00 +0530

    About Us page added to collapse menu in action bar

---
+++ OCRScannerApp/app/src/main/java/OCR/OCRScanner/aboutus.java
@@ -0,0 +1,16 @@
+package OCR.OCRScanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mihinduranasinhe.OCR.R;
+
+public class aboutus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+    }
+}
